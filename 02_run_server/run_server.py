c42d9b6fbcf0a56fc37ba0039b821211eaaa203c
bumzack <bumzacktrara@gmail.com>
2020-10-06 21:17:46 +0200

    hahaha - how to load a styles css file using flask - wtf

--- 02_run_server/run_server.py
+++ 02_run_server/run_server.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 import nltk
@@ -16,10 +17,10 @@ intents = json.loads(open('../input_data/intents.json').read())
 words = pickle.load(open('../data/words.pkl', 'rb'))
 classes = pickle.load(open('../data/classes.pkl', 'rb'))
 
+STATIC_DIR = os.path.abspath('../02_run_server/static')
+print("STATIC DIR   ", STATIC_DIR)
 
-
-
-app = Flask(__name__)
+app = Flask(__name__, static_folder=STATIC_DIR)
 
 
 def clean_up_sentence(sentence):
